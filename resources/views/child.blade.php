7f934b3c02a8f93f6507d4ed9d5c349e998eec1c
Nathaniel Pather <nathaniel_pather@hotmail.com>
2020-02-03 22:06:07 +1000

    Change post form visibility for unauthenticated users.

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -3,39 +3,36 @@
 @section('title', 'Page Title')
 
 @section('form')
-    @if(Session::has('success'))
-        <div class="alert alert-success">
-            {{ Session::get('success') }}
-            @php
-                Session::forget('success');
-            @endphp
-        </div>
+    @if(auth()->check())
+        <form method="POST" action="{{ url('/') }}">
+
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label>User:</label>
+                <input type="text" name="user" placeholder="Enter your username">
+                @if ($errors->has('user'))
+                    <span>{{ $errors->first('user') }}</span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label>Post:</label>
+                <input type="text" name="post" placeholder="Enter a post">
+                @if ($errors->has('post'))
+                    <span>{{ $errors->first('post') }}</span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <button>Submit</button>
+            </div>
+        </form>
+    @else
+        <p>You must be logged in to create a post.</p>
+        <p>If you own an account, please log into it.</p>
+        <p>Otherwise create a new account.</p>
     @endif
-
-    <form method="POST" action="{{ url('/') }}">
-
-        {{ csrf_field() }}
-
-        <div class="form-group">
-            <label>User:</label>
-            <input type="text" name="user" placeholder="Enter your username">
-            @if ($errors->has('user'))
-                <span>{{ $errors->first('user') }}</span>
-            @endif
-        </div>
-
-        <div class="form-group">
-            <label>Post:</label>
-            <input type="text" name="post" placeholder="Enter a post">
-            @if ($errors->has('post'))
-                <span>{{ $errors->first('post') }}</span>
-            @endif
-        </div>
-
-        <div class="form-group">
-            <button>Submit</button>
-        </div>
-    </form>
 @endsection
 
 @section('posts')
